8d6c7465dc235698ddd241ac131974e7ae4af31a
Gilmer Contreras <ivsmca90@gmail.com>
2025-08-01 00:20:03 -0400

    Update

--- middlewares/validateHost.js
+++ middlewares/validateHost.js
@@ -1,9 +1,9 @@
 module.exports = function (req, res, next) {
   const allowedOrigins = [
-    'https://rifari.com',
-    'https://www.rifari.com',
-    'https://cloud.rifari.com',
-    'https://www.cloud.rifari.com',
+    'https://rifacaracasyei.com',
+    'https://www.rifacaracasyei.com',
+    'https://admin.rifacaracasyei.com',
+    'https://www.admin.rifacaracasyei.com',
     // 'https://ganaconbeltran.com',
     // 'https://www.ganaconbeltran.com',
     // 'https://admin.ganaconbeltran.com',
