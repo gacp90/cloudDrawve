9423c68d058e3551f3bf09157fd9aaf88bfa6547
Gilmer Contreras <ivsmca90@gmail.com>
2025-08-05 22:32:55 -0400

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const validateHost = require('./middlewares/validateHost');
+// const validateHost = require('./middlewares/validateHost');
 
 //Conection DB
 const { dbConection } = require('./database/config');
